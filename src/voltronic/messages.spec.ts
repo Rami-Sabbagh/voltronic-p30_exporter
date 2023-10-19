db6b766cde90325f228482eecc43535ca722386f
Rami Sabbagh <rami.sab07@gmail.com>
2023-10-20 00:49:50 +0300

    Implement messages packing and unpacking

---
+++ src/voltronic/messages.spec.ts
@@ -0,0 +1,58 @@
+import { packMessage, unpackMessage } from './messages';
+
+test.each([
+    ['QPI', '51 50 49 be ac 0d'],
+    ['VOLTRONICS', '56 4f 4c 54 52 4f 4e 49 43 53 6c 3e 0d'],
+    ['#$!@-,;:[]/', '23 24 21 40 2d 2c 3b 3a 5b 5d 2f e1 a2 0d']
+])('packMessage("%s") is [%s]', (message: string, hex: string) => {
+    const expected = Buffer.from(hex.replaceAll(' ', ''), 'hex');
+    const actual = packMessage(message);
+
+    expect(actual).toStrictEqual(expected);
+});
+
+test.each([
+    ['51 50 49 BE AC 0D', 'QPI'],
+    ['56 4f 4c 54 52 4f 4e 49 43 53 6c 3e 0d', 'VOLTRONICS'],
+    ['23 24 21 40 2d 2c 3b 3a 5b 5d 2f e1 a2 0d', '#$!@-,;:[]/'],
+])('unpackMessage([%s]) is "%s"', (hex: string, message: string) => {
+    const data = Buffer.from(hex.replaceAll(' ', ''), 'hex');
+    const actual = unpackMessage(data);
+
+    expect(actual).toStrictEqual(message);
+});
+
+test('Checksum fails with a missing byte', () => {
+    const hex = '50 49 BE AC 0D' // QPI but missing the first byte.
+    const data = Buffer.from(hex.replaceAll(' ', ''), 'hex');
+    
+    expect(() => unpackMessage(data)).toThrow(/^Checksum mismatch:/g);
+});
+
+test('Checksum fails with a corrupted byte', () => {
+    const hex = '51 B9 49 BE AC 0D' // QPI but with P messed up.
+    const data = Buffer.from(hex.replaceAll(' ', ''), 'hex');
+    
+    expect(() => unpackMessage(data)).toThrow(/^Checksum mismatch:/g);
+});
+
+test('Missing carriage return is rejected', () => {
+    const hex = '51 50 49 BE AC' // QPI missing the carriage return.
+    const data = Buffer.from(hex.replaceAll(' ', ''), 'hex');
+    
+    expect(() => unpackMessage(data)).toThrow('Missing carriage return.');
+});
+
+test.each([
+    ['Hello World!'],
+    ['The Little Quick Brown Fox Jumps Over The Lazy Dog.'],
+])('"%s" packs, unpacks and repacks as expected', (message: string) => {
+    const packed = packMessage(message);
+    expect(packed).toMatchSnapshot();
+
+    const unpacked = unpackMessage(packed);
+    expect(unpacked).toStrictEqual(message);
+
+    const repacked = packMessage(unpacked);
+    expect(repacked).toStrictEqual(packed);
+});
---
+++ src/voltronic/messages.ts
@@ -0,0 +1,42 @@
+import { crc16Xmodem } from './crc16-xmodem';
+
+/**
+ * Packs the message into the transmission ready format of voltronics.
+ * 
+ * Calculates the checksum of the message and appends it to the end of the message
+ * alongside a carriage return as specified by the protocol.
+ */
+export function packMessage(message: string): Buffer {
+    const result = Buffer.alloc(message.length + 3);
+    result.write(message, 'ascii');
+
+    const checksum = crc16Xmodem(result.subarray(0, -3));
+    result.writeUInt16BE(checksum, message.length);
+    result.write('\r', result.length - 1, 'ascii');
+
+    return result;
+}
+
+/**
+ * Unpack the message from the transmitted format.
+ * 
+ * Validates the checksum and strips it alongside the carriage return from the
+ * message as specified by the protocol.
+ * 
+ * Also removes the first character from the message if it was `(`.
+ */
+export function unpackMessage(data: Buffer): string {
+    if (data.at(-1) !== 0x0D) throw 'Missing carriage return.';
+
+    const expected = data.readUInt16BE(data.length - 3);
+    const actual = crc16Xmodem(data.subarray(0, data.length - 3));
+
+    if (expected !== actual)
+        throw 'Checksum mismatch: ' +
+            `expected 0x${expected.toString(16)}, ` +
+            `got 0x${actual.toString(16)}.`;
+
+    return data.toString('ascii',
+        data.at(0) === 0x28 ? 1 : 0, // '(' character.
+        data.length - 3);
+}
