91393743f5565a3710c6ce5b57bc65e73596728e
Rami Sabbagh <rami.sab07@gmail.com>
2023-10-20 16:30:43 +0300

    Implement command timeout

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ const protocol = new VoltronicProtocol('COM4');
 const api = new VoltronicAPI(protocol);
 
 (async () => {
-    console.log('Protocol:', await api.queryProtocolId());
-    console.log('FW Version:', await api.queryFirmwareVersion());
-    console.log('Serial:', await api.querySerialNumber());
-    console.log('Model:', await api.queryModelName());
-    console.log('Device Mode:', await api.queryDeviceMode());
-    console.log('Parameters:', await api.queryGeneralStatusParameters());
+    console.log('Protocol:', await api.queryProtocolId().catch((error) => `${error}`));
+    console.log('FW Version:', await api.queryFirmwareVersion().catch((error) => `${error}`));
+    console.log('Serial:', await api.querySerialNumber().catch((error) => `${error}`));
+    console.log('Model:', await api.queryModelName().catch((error) => `${error}`));
+    console.log('Device Mode:', await api.queryDeviceMode().catch((error) => `${error}`));
+    console.log('Parameters:', await api.queryGeneralStatusParameters().catch((error) => `${error}`));
 
     protocol.destroy();
 })().catch(console.error).finally(() => protocol.destroy());
--- src/voltronic/protocol.ts
+++ src/voltronic/protocol.ts
@@ -5,6 +5,21 @@ import { Mutex } from 'async-mutex';
 import { packMessage, unpackMessage } from './messages';
 import { delay } from './utilities';
 
+export interface VoltronicProtocolOptions {
+    /**
+     * Milliseconds to wait before sending a command.
+     */
+    delay?: number,
+    /**
+     * Milliseconds before giving up on a response arriving.
+     */
+    timeout?: number,
+}
+
+const defaultOptions = {
+    delay: 50,
+    timeout: 2000,
+} satisfies Required<VoltronicProtocolOptions>;
 
 export class VoltronicProtocol {
     protected stream: Duplex;
@@ -22,15 +37,15 @@ export class VoltronicProtocol {
      * @param path The system path of the serial port you want to open.
      * For example, `/dev/tty.XXX` on Mac/Linux, or `COM1` on Windows
      */
-    constructor(path: string);
+    constructor(path: string, options?: VoltronicProtocolOptions);
 
     /**
      * Communicate with a voltronic device regardless
      * of the transport used.
      */
-    constructor(stream: Duplex);
+    constructor(stream: Duplex, options?: VoltronicProtocolOptions);
 
-    constructor(source: string | Duplex) {
+    constructor(source: string | Duplex, protected readonly options: VoltronicProtocolOptions = {}) {
         // FIXME: Handle transport failure and create a new protocol instance.
         this.stream = (typeof source === 'string')
             ? new SerialPort({ path: source, baudRate: 2400 })
@@ -49,13 +64,13 @@ export class VoltronicProtocol {
     async execute(command: string, raw: true): Promise<Buffer>;
     async execute(command: string, raw?: boolean): Promise<string | Buffer> {
         return this.mutex.runExclusive(async () => {
-            await delay(1000);
+            await delay(this.option('delay'));
 
             // Discard any content in the parser buffer.
             this.parser.buffer = Buffer.alloc(0);
             this.stream.write(packMessage(command));
 
-            const data = await this.read();
+            const data = await this.read(this.option('timeout'));
 
             if (raw) {
                 const response = unpackMessage(data, true);
@@ -73,23 +88,37 @@ export class VoltronicProtocol {
         this.stream.destroy();
     }
 
-    protected async read(): Promise<Buffer> {
+    protected async read(timeout?: number): Promise<Buffer> {
         return new Promise((resolve, reject) => {
             let onData: (data: Buffer) => void;
             let onError: (error: unknown) => void;
+            let timeoutId: NodeJS.Timeout | undefined;
 
             onData = (data) => {
+                clearTimeout(timeoutId);
                 this.parser.off('error', onError);
                 resolve(data);
             };
 
             onError = (error) => {
+                clearTimeout(timeoutId);
                 this.parser.off('data', onData);
                 reject(error);
             };
 
+            if (timeout !== undefined)
+                timeoutId = setTimeout(() => {
+                    this.parser.off('data', onData);
+                    this.parser.off('error', onError);
+                    reject('Timeout');
+                }, timeout);
+
             this.parser.once('data', resolve);
             this.parser.once('error', reject);
         });
+    }
+
+    protected option<T extends keyof VoltronicProtocolOptions>(key: T): Required<VoltronicProtocolOptions>[T] {
+        return this.options[key] ?? defaultOptions[key];
     }
 }
